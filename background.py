0e7cbed1de3a5edad54d3718bba2834227fe7e1b
atikadefs <153360108+atikadefs@users.noreply.github.com>
2023-12-14 11:03:10 +0700

    Update background.py

--- background.py
+++ background.py
@@ -1,18 +1,20 @@
 import streamlit as st
 
-# Menambahkan CSS untuk latar belakang
-st.markdown(
-    """
-    <style>
-    body {
-        background-image: url('https://github.com/atikadefs/streamlit-example/blob/master/Cone%20Nebula%20(NGC2264).jpeg');
-        background-size: cover;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+# Mengimpor gambar dari lokal
+from PIL import Image
 
-# Konten aplikasi Streamlit
-st.title('Aplikasi dengan Latar Belakang')
-st.write('Konten aplikasi Streamlit di sini...')
+# Menampilkan gambar dari URL
+from io import BytesIO
+import requests
+
+# Menambahkan gambar dari URL
+url = 'https://github.com/atikadefs/streamlit-example/blob/master/Cone%20Nebula%20(NGC2264).jpeg'
+response = requests.get(url)
+img = Image.open(BytesIO(response.content))
+
+# Menampilkan gambar dari lokal
+local_image = Image.open('nama_file_gambar_lokal.jpg')
+
+# Menampilkan gambar di aplikasi Streamlit
+st.image(img, caption='Gambar dari URL', use_column_width=True)
+st.image(local_image, caption='Gambar dari lokal', use_column_width=True)
