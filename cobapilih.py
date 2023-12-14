ea092a833798a0f054c4be8468f5d6475fdff0f3
atikadefs <153360108+atikadefs@users.noreply.github.com>
2023-12-14 11:58:46 +0700

    Update cobapilih.py

--- cobapilih.py
+++ cobapilih.py
@@ -5,8 +5,8 @@ pilihan = st.radio("Pilih satu:", ("Satu", "Dua"))
 
 if pilihan == "Satu":
     # Menampilkan gambar jika 'Satu' dipilih
-    st.image('link_gambar_di_sini', caption='Gambar yang ditampilkan')
+    st.image('https://github.com/atikadefs/streamlit-example/blob/master/Cone%20Nebula%20(NGC2264).jpeg', caption='Gambar yang ditampilkan')
 
 elif pilihan == "Dua":
     # Menampilkan video jika 'Dua' dipilih
-    st.video('link_video_di_sini', start_time=0)
+    st.video('https://github.com/atikadefs/streamlit-example/blob/master/Video%20tanpa%20judul%20%E2%80%90%20Dibuat%20dengan%20Clipchamp%20(1)%20(1).mp4', start_time=0)
