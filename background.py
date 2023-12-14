02ee79e4ed82e2d7b6d37d681560bbd8cbc25d9e
atikadefs <153360108+atikadefs@users.noreply.github.com>
2023-12-14 11:00:08 +0700

    Update background.py

--- background.py
+++ background.py
@@ -1,18 +1,18 @@
 import streamlit as st
 
-def main():
-    # Menggunakan HTML dan CSS kustom untuk mengatur gambar latar belakang
-page_bg_img = '''
-<style>
-.stApp {
-    background-image: url("https://github.com/atikadefs/streamlit-example/blob/master/Cone%20Nebula%20(NGC2264).jpeg");
-    background-size: cover;
-}
-</style>
-'''
+# Menambahkan CSS untuk latar belakang
+st.markdown(
+    """
+    <style>
+    body {
+        background-image: url('https://github.com/atikadefs/streamlit-example/blob/master/Cone%20Nebula%20(NGC2264).jpeg');
+        background-size: cover;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
-    st.title('Aplikasi dengan Background Gambar')
-    st.write('Ini adalah konten aplikasi dengan background gambar.')
-
-if __name__ == "__main__":
-    main()
+# Konten aplikasi Streamlit
+st.title('Aplikasi dengan Latar Belakang')
+st.write('Konten aplikasi Streamlit di sini...')
