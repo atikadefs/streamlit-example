9583619be9c19edbf977de1ed1f1246acac895b1
atikadefs <153360108+atikadefs@users.noreply.github.com>
2023-12-14 10:54:32 +0700

    Update background.py

--- background.py
+++ background.py
@@ -1,18 +1,15 @@
 import streamlit as st
 
 def main():
-    # Menambahkan CSS untuk mengatur gambar sebagai background
-    st.markdown(
-        """
-        <style>
-        body {
-            background-image: url('https://github.com/atikadefs/streamlit-example/blob/master/Cone%20Nebula%20(NGC2264).jpeg/'); /* Ganti URL dengan URL gambar Anda */
-            background-size: cover;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
+    # Menggunakan HTML dan CSS kustom untuk mengatur gambar latar belakang
+page_bg_img = '''
+<style>
+.stApp {
+    background-image: url("https://raw.githubusercontent.com/enjunaja/belajar-vektor/main/gambar/background.png");
+    background-size: cover;
+}
+</style>
+'''
 
     st.title('Aplikasi dengan Background Gambar')
     st.write('Ini adalah konten aplikasi dengan background gambar.')
